f04f3f8d0848e701761d0635fb0514f499e3710b
AbdalrhmanJuber <1211769@student.birzeit.edu>
2025-06-02 23:39:19 +0300

    Add: login&signup

--- app/src/main/java/com/example/a1211769_courseproject/MainActivity.java
+++ app/src/main/java/com/example/a1211769_courseproject/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.a1211769_courseproject;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String jsonData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +24,30 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Get the JSON data from the intent (passed from Welcome_layout)
+        if (getIntent().hasExtra("jsonData")) {
+            jsonData = getIntent().getStringExtra("jsonData");
+        }
+
+        // Load the login fragment by default
+        if (savedInstanceState == null) {
+            loadFragment(new LoginFragment());
+        }
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
+    }
+
+    // Called when login is successful
+    public void onLoginSuccess(String email) {
+        Toast.makeText(this, "Welcome, " + email, Toast.LENGTH_SHORT).show();
+        // TODO: Navigate to the property listing screen or dashboard
+        // For now, just display a toast
     }
 }
+
